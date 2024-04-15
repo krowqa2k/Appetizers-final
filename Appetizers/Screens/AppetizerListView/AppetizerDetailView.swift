d24b3e1a3eb411496790b4389922b2fb5d9fe0e2
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-04-15 20:01:48 +0200

    detail view

---
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -0,0 +1,105 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by admin on 15/04/2024.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        VStack{
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 320, height: 225)
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                    .foregroundStyle(.secondary)
+                
+                HStack(spacing: 40) {
+                    VStack(spacing: 5) {
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                            
+                        Text("\(appetizer.calories)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Carbs")
+                            .bold()
+                            .font(.caption)
+                            
+                        Text("\(appetizer.carbs)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Protein")
+                            .bold()
+                            .font(.caption)
+                            
+                        Text("\(appetizer.protein)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                print("tapped")
+            } label: {
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(10)
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(Button {
+            print("dismiss")
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .opacity(0.6)
+                
+                Image(systemName: "xmark")
+                    .frame(width: 44, height: 44)
+                    .foregroundColor(.black)
+            }
+        },  alignment: .topTrailing)
+    }
+}
+
+#Preview {
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+}
--- Appetizers/View/Cells/AppetizerListCell.swift
+++ Appetizers/View/Cells/AppetizerListCell.swift
@@ -17,7 +17,7 @@ struct AppetizerListCell: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 120, height: 90)
                 .cornerRadius(8)
-            
+                    
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
                     .font(.title2)
